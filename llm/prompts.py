fd5e26d0daacc8ca6bc351fd2c7335bb729fa9a6
SayantanHutait <sayantan344hutait@gmail.com>
2026-04-06 20:19:20 +0530

    fix(deps): import PromptTemplate from langchain_core for v0.3 compat

--- llm/prompts.py
+++ llm/prompts.py
@@ -1,5 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 import json
